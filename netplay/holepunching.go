841cedf4d1f807ad3acc7df47cafb07b8a91de90
Jean-André Santoni <jean.andre.santoni@gmail.com>
2023-04-04 11:36:13 +0200

    New server address

--- netplay/holepunching.go
+++ netplay/holepunching.go
@@ -127,7 +127,7 @@ func makeJoinPacket() []byte {
 // UDPHolePunching attempt to traverse the NAT
 func UDPHolePunching() error {
 	rdv, err := net.DialUDP("udp", nil, &net.UDPAddr{
-		IP:   net.ParseIP("127.0.0.1"),
+		IP:   net.ParseIP("13.50.239.171"),
 		Port: 1234,
 	})
 	if err != nil {
